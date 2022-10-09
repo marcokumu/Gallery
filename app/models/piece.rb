8c8fca6e88eaa0841ab3c53fe1db167adb2221f1
Okumu Mark <marcokumu10@gmail.com>
2022-10-09 07:58:55 +0000

    piece relationship and validations added

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,2 +1,9 @@
 class Piece < ApplicationRecord
+    validates :image, presence: true
+    validates :title, presence: true, length: { in: 1..30 }
+    validates :medium, presence: true, length: { in: 1..50}
+    validates :description, length: { maximum: 500}
+    validates :worth, numericality: true
+    belongs_to :user
+    belongs_to :gallery
 end
