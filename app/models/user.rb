963e120f2f05e98259c796ab3a959b5de5812535
Okumu Mark <marcokumu10@gmail.com>
2022-10-09 07:59:34 +0000

    user reletionships and validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: { in: 6..15 }
+    validates :password_digest, presence: true
+    validates :bio, length: { maximum: 500 }
+    has_many :pieces, dependent: :destroy
+    has_many :galleries, through: :pieces
+    has_secure_password
 end
