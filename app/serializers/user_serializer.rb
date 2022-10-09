2edd0283b15498824e753d3d3fa6103138d941db
Okumu Mark <marcokumu10@gmail.com>
2022-10-09 08:02:40 +0000

    user serializer code added

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password_digest, :image_url, :bio
+  has_many :pieces, dependent: :destroy
+  has_many :galleries, through: :pieces
 end
