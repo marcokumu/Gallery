580882684056fbc03ceb2a6773bec8ee41dc73d4
Okumu Mark <marcokumu10@gmail.com>
2022-10-09 07:43:47 +0000

    Galleries resource generated

---
+++ app/serializers/gallery_serializer.rb
@@ -0,0 +1,3 @@
+class GallerySerializer < ActiveModel::Serializer
+  attributes :id, :name, :address
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :galleries
   resources :pieces
   resources :users
   # Routing logic: fallback requests for React Router.
