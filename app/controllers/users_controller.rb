c9f6172ed82a55e72f1865393e342cb55f72b36f
Okumu Mark <marcokumu10@gmail.com>
2022-10-09 07:57:26 +0000

    user controller code created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+skip_before_action :authorize, only: :create
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def index
+        users = User.all
+        render json: users, include: :pieces, status: :ok
+    end
+
+    def show
+        render json: @current_user, include: :pieces, status: :ok
+    end
+
+    def destroy
+        user = @current_user
+        user.destroy
+        head :no_content
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+
 end
