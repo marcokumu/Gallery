697d1b02b9f1781fa21fa3a5e8991ba629a0838e
Okumu Mark <marcokumu10@gmail.com>
2022-10-09 07:53:06 +0000

    galleries controller code added

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,2 +1,13 @@
 class GalleriesController < ApplicationController
+    skip_before_action :authorize, only: :index
+
+    def index
+        render json: Gallery.all, status: :ok
+    end
+
+    def show
+        gallery = Gallery.find(params[:id])
+        render json: gallery, status: :ok
+    end
+
 end
