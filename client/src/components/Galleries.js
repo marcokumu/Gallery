b841254e7845264f1d419c84479f936962b0882e
Okumu Mark <marcokumu10@gmail.com>
2022-10-09 08:22:40 +0000

    Galleries code added

---
+++ client/src/components/Galleries.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import styles from "../appStyles.module.css";
+
+function Galleries({ onFetchGalleries, galleries, onGalleryClick }) {
+  useEffect(() => {
+    fetch("/galleries").then((r) => {
+      if (r.ok) {
+        r.json().then((venues) => onFetchGalleries(venues));
+      } else {
+        r.json().then((err) => console.log(err.errors));
+      }
+    });
+  }, []);
+
+  return (
+    <div className={styles.gallery_container}>
+      {galleries.map((gallery) => {
+        return (
+          <div
+            className={styles.gallery_name}
+            key={gallery.name}
+            onClick={() => onGalleryClick(gallery)}
+          >
+            <p>{gallery.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Galleries;
