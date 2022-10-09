44335f2df9693deec07b7ada9175dc5b5abd2e02
Okumu Mark <marcokumu10@gmail.com>
2022-10-09 07:48:10 +0000

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Gallery.create!([{
+    name: "Ogden Contemporary Arts",
+    address: '455 25th St, Ogden, UT 84401'
+},
+{
+    name: "Soulard Art Gallery",
+    address: '2028 s 12th St, St. Louis, MO 63104'
+}, 
+{
+    name: 'Yale University Art Gallery',
+    address: "1111 Chapel St, New Haven, CT 06510"
+},
+{
+    name: 'The Sheldon Concert Hall',
+    address: '3648 Washington Ave, St. Louis, MO 63108'
+},
+{
+    name: 'Contemporary Art Museum of St. Louis',
+    address: '3750 Washington Ave, St. Louis, MO 63108'
+},
+{
+    name: 'Lehigh University Art Galleries',
+    address: '420 E Packer Ave, Bethlehem, PA 18015'
+}]
+)
